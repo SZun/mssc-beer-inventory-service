e51b687379f6acdb2dab719ed2406765c6c90b2a
Zun <zunsamuel97@gmail.com>
2020-09-07 10:34:20 -0500

    step 197

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,33 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.BeerDto;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    private void listen(NewInventoryEvent event){
+        log.debug("Got Inventory -> {}", event.toString());
+
+        BeerDto beerDto = event.getBeerDto();
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(beerDto.getId())
+                .upc(beerDto.getUpc())
+                .quantityOnHand(beerDto.getQuantityOnHand())
+                .build());
+    }
+
+}
